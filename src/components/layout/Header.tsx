42047b015b3885842d87dea7e598e0c2b45f62e4
Anton029040 <2002anton02@gmail.com>
2026-05-19 22:27:01 +0200

    Header implemented
    Implemented a header that contains a logo and a search bar. The header is responsive and has breakpoints at 900 and 500

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,20 @@
+import SearchBar from "../ui/SearchBar";
+import "./Header.css";
+
 // This is for the page Header, it includes a logo and a searchfield
 const Header = () => {
     return(
-        <header>
-            <h1> Placeholder </h1>
-            { /* placeholder for testing */ }
-        </header>
+        <div className = "header">
+            <div className = "inner-header">
+                <div className="logo-block">
+                    <img src="../../assets/book-logo.svg" className="logo"/>
+                    <h1 className="title">Booklog</h1>
+                </div>
+                <div className="searchbar-wrapper">
+                    <SearchBar></SearchBar>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-// this is for the navigation bar ( our 3 tabs that navigate to new endpoints )
 const Navbar = () => {
     return(
         <nav>
@@ -11,8 +10,6 @@ const Navbar = () => {
             <NavLink to="/tbr">
                 TBR
             </NavLink>
-
-            {/* other links need to be added, this is for testing purposes - Natalie */}
         </nav>
     );
 };
