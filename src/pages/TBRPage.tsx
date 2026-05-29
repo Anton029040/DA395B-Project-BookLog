6bb325100c75c632d295bc652ba4117cd1fe7667
Anton029040 <2002anton02@gmail.com>
2026-05-29 09:12:46 +0200

    Fix - search
    You can now search for books in TBR

--- src/pages/TBRPage.tsx
+++ src/pages/TBRPage.tsx
@@ -12,7 +12,7 @@ import "./TBRPage.css"
 
 const TBRPage = () => {
     const [searchParams] = useSearchParams();
-    const query = searchParams.get("search");
+    const query = searchParams.get("query");
     const [ booksInTBR, setBooksInTBR ] = useState<ApiBook[]>([]);
 
     const {                                                     // destructured values/functions from the hook
